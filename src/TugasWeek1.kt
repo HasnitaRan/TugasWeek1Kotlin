c1b21adaf8168cd591c7ff47df34fea92e449d16
HasnitaRan <hasnitaranikumala@gmail.com>
2021-12-12 09:05:39 +0700

    TugasWeek1Plugin

---
+++ src/TugasWeek1.kt
@@ -0,0 +1,11 @@
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println
+
+fun main() {
+    var biodata: String = """Nama  : Hasnita Rani Kumala
+        Kelas : 1C
+        NIM   : 21090092
+        Prodi : Teknik Informatika
+        """
+    println(biodata)
+}
+
